cccd9419d7d1d48b651297d84d1e74d739372ea2
rishab1128 <rishabpurkayastha28@gmail.com>
2021-06-14 17:37:57 +0530

    Animations for the buttons added

--- Day-1-Drum-Kit/app.js
+++ Day-1-Drum-Kit/app.js
@@ -1,9 +1,3 @@
-const key=document.querySelectorAll('.key');
-console.log(key);
-
-const keys=document.querySelector('.keys');
-console.log(keys);
-
 const audios=document.getElementsByTagName('audio');
 //console.log(audios[0].dataset.key);
 
@@ -23,12 +17,36 @@ function runEvent(e){
         }
     }*/
 
-    //Method-2
-    const audio=document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    //Method-2 --> Without using loop
+    const audio=document.querySelector(`audio[data-key="${e.keyCode}"]`); //Selecting the audio element which matches with the corresponding keyCode
+
+    const key=document.querySelector(`.key[data-key="${e.keyCode}"]`);//Selecting the actual element with class=key which matches with the corresponding keyCode for adding the button animations
+
+
     console.log(audio);
+    console.log(key);
+    console.log(key.classList);
+
+    //For adding animation -> class='playing gets added in the div(->classList) of that corresponding element when we press the key
+    key.classList.add('playing');
+
+
     if(!audio)
         return ; //stops the func from running all together
     audio.currentTime=0; //rewind to the start
     audio.play();
 
+}
+
+//For removing the transition that gets added in the button when we press the key
+const keys=document.querySelectorAll('.key'); //returns a Node List
+console.log(keys);
+
+keys.forEach((key)=>{
+    key.addEventListener('transitionend',removeTransition);
+})
+
+function removeTransition(e){
+    console.log(e , e.target);
+    e.target.classList.remove('playing');
 }
